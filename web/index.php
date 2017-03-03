ad56701306fc8b859f705e0add7351527a9d212f
ValentinLipatov <valentin-ltd@mail.ru>
2017-03-03 16:08:39 +0300

    Add files via upload

---
+++ web/index.php
@@ -0,0 +1,55 @@
+<?php
+$dbconn = pg_connect("
+	host     = ec2-176-34-111-152.eu-west-1.compute.amazonaws.com
+	dbname   = ddironoubdcdin
+	user     = zaqnsiwfubvdlf
+	password = dfd29c90a1108174bde464b59bea334b3eacc604eb29b2c490dec86cee54d481
+")or die('Could not connect: ' . pg_last_error());
+
+switch ($_GET["comand"])
+{
+    case "create": 
+    {
+        try 
+        {  
+            $query = "CREATE TABLE messages (message TEXT NOT NULL)";
+            $result = pg_query($query) or die(pg_last_error());
+            echo "SUCCESS table users is created";
+        } 
+        catch (Exception $e) 
+        {
+            echo "ERROR: Database are not created.";
+        }    
+    } break; 
+    
+    case "get": 
+    {
+		$query = "SELECT * FROM messages";
+		$result = pg_query($query) or die(pg_last_error());
+		while ($line = pg_fetch_array($result, null, PGSQL_ASSOC))
+		{
+			echo $line["message"];
+		}
+				
+    } break;
+	
+    case "set": 
+    {
+		$query = "INSERT INTO messages (message) VALUES ('$_GET[text]')";
+		$result = pg_query($query) or die(pg_last_error());
+    } break;
+	
+	case "delete": 
+    {
+        $query = "DELETE * FROM messages";
+		$result = pg_query($query) or die(pg_last_error());
+        
+    } break;
+    default: 
+    {
+        echo "ERROR: Unknow comand.";
+    } break;
+}
+pg_free_result($result);
+pg_close($dbconn);
+?>
